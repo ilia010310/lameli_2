e30f775003fdd136a814e421872283ba41644a6c
Ilia <Theilyaboyarintsev@gmail.com>
2024-02-12 17:36:24 -0300

    cart's view is throw classes

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from . import views
+from .views import CartAddView, CartRemoveView, CartDetailView
 
 app_name = 'cart'
 
 urlpatterns = [
-    path('add/<int:product_id>/', views.cart_add, name='cart_add'),
-    path('remove/<int:product_id>/', views.cart_remove, name='cart_remove'),
-    path('', views.cart_detail, name='cart_detail'),
-
+    path('add/<int:product_id>/', CartAddView.as_view(), name='cart_add'),
+    path('remove/<int:product_id>/', CartRemoveView.as_view(), name='cart_remove'),
+    path('detail/', CartDetailView.as_view(), name='cart_detail'),
 ]
--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,15 +1,16 @@
-
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
 from django.views.decorators.http import require_POST
 from .cart import Cart
 from .forms import CartAddProductForm
 from ..product.models import Product
 
 
-@require_POST
-def cart_add(request, product_id):
-    cart = Cart(request)
-    if cart:
+class CartAddView(View):
+    @staticmethod
+    @require_POST
+    def post(request, product_id):
+        cart = Cart(request)
         product = get_object_or_404(Product, id=product_id)
 
         form = CartAddProductForm(request.POST)
@@ -19,20 +20,22 @@ def cart_add(request, product_id):
                      quantity=cd['quantity'],
                      update_quantity=cd['update'])
         return redirect('cart:cart_detail')
-    else:
-        pass
 
 
-def cart_remove(request, product_id):
-    cart = Cart(request)
-    product = get_object_or_404(Product, id=product_id)
-    cart.remove(product)
-    return redirect('cart:cart_detail')
+class CartRemoveView(View):
+    @staticmethod
+    def post(request, product_id):
+        cart = Cart(request)
+        product = get_object_or_404(Product, id=product_id)
+        cart.remove(product)
+        return redirect('cart:cart_detail')
 
 
-def cart_detail(request):
-    cart = Cart(request)
-    for item in cart:
-        item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'],
-                                                                   'update': True})
-    return render(request, 'cart/detail.html', {'cart': cart})
+class CartDetailView(View):
+    @staticmethod
+    def get(request):
+        cart = Cart(request)
+        for item in cart:
+            item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'],
+                                                                       'update': True})
+        return render(request, 'cart/detail.html', {'cart': cart})
